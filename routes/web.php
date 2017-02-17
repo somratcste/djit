96d5e29c4808ffed0047a282af4bdc2d0141a8e7
somratcste <somratcste@gmail.com>
2017-02-17 12:32:46 +0600

    After login route redirect

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::get('/home', function () {
+    return redirect('/forums');
+});
+
+//Route::get('/home', 'HomeController@index');
