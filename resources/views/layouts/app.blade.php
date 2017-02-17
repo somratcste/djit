252187f881a23fc55de2e174fbe33035f5112faa
somratcste <somratcste@gmail.com>
2017-02-17 12:39:36 +0600

    User name fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
                             <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style="text-transform: capitalize;"></style>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
