484435d353c9573c8e815606515bb1e2075ef5f9
somratcste <somratcste@gmail.com>
2017-02-18 11:52:57 +0600

    fix minified css

--- vendor/devdojo/chatter/src/Views/discussion.blade.php
+++ vendor/devdojo/chatter/src/Views/discussion.blade.php
@@ -1,8 +1,8 @@
 @extends(Config::get('chatter.master_file_extend'))
 
 @section(Config::get('chatter.yields.head'))
-	<link href="{{ url('/') }}./vendor/devdojo/chatter/assets/css/chatter.css" rel="stylesheet">
-	<link href="{{ url('/') }}./vendor/devdojo/chatter/assets/css/simplemde.min.css" rel="stylesheet">
+	<link href="{{ url('/') }}/vendor/devdojo/chatter/assets/css/chatter.css" rel="stylesheet">
+	<link href="{{ url('/') }}/vendor/devdojo/chatter/assets/css/simplemde.min.css" rel="stylesheet">
 @stop
 
 
