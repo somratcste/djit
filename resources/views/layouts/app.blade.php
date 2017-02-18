ccae6f9bf4c760116eb7e12451025ae3c1a2b439
somratcste <somratcste@gmail.com>
2017-02-18 10:41:36 +0600

    fix title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Forums || Home</title>
+    <title>Forums || Win or Learn , Never Lose</title>
 
     <!-- Styles -->
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
